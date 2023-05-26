dcb3b59d3542ebc205021e5d8911ca9dbf1f0a53
Javier Sosa <jsosa@globaluy.com>
2023-05-26 18:28:05 -0300

    Feature - Add SafeAreaProvider and CustomStatusBar to combine the application with the elements of the cell phone

--- App.tsx
+++ App.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
-import AppNavigator from './navigation';
-
 import { default as theme } from './theme.json';
 import AuthNavigator from './navigation/AuthNavigator';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import { StatusBar, View } from 'react-native';
+import React from 'react';
+
+type CustomStatusBarProps = {
+  backgroundColor: string;
+  barStyle: any;
+};
+
+const CustomStatusBar = ({
+  backgroundColor,
+  barStyle,
+}: //add more props StatusBar
+CustomStatusBarProps): React.ReactElement => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View style={{ height: insets.top, backgroundColor }}>
+      <StatusBar
+        animated={true}
+        backgroundColor={backgroundColor}
+        barStyle={barStyle}
+      />
+    </View>
+  );
+};
 
 export default () => {
   const queryClient = new QueryClient();
@@ -15,16 +41,26 @@ export default () => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider
-        {...eva}
-        theme={
-          darkMode ? { ...eva.dark, ...theme } : { ...eva.light, ...theme }
-        }
-      >
-        {/* <AppNavigator /> */}
-        <AuthNavigator />
-      </ApplicationProvider>
+      <SafeAreaProvider>
+        {/* <StatusBar
+        barStyle={darkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={darkMode ? '#000000' : '#FFFFFF'}
+      /> */}
+        <CustomStatusBar
+          barStyle={darkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={darkMode ? '#000000' : '#FFFFFF'}
+        />
+        <IconRegistry icons={EvaIconsPack} />
+        <ApplicationProvider
+          {...eva}
+          theme={
+            darkMode ? { ...eva.dark, ...theme } : { ...eva.light, ...theme }
+          }
+        >
+          {/* <AppNavigator /> */}
+          <AuthNavigator />
+        </ApplicationProvider>
+      </SafeAreaProvider>
     </QueryClientProvider>
   );
 };
