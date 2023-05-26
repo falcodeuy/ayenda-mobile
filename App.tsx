19aa32a70a2182946bb1d82cc103da03bdaa89ef
Javier Sosa <jsosa@globaluy.com>
2023-05-26 18:28:05 -0300

    Feature - Add SafeAreaProvider and CustomStatusBar to combine the application with the elements of the cell phone

--- App.tsx
+++ App.tsx
@@ -1,18 +1,49 @@
-import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
-import AppNavigator from './navigation';
-
 import { default as theme } from './theme.json';
 import AuthNavigator from './navigation/AuthNavigator';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { StatusBar, View } from 'react-native';
+import React from 'react';
+
+type CustomStatusBarProps = {
+  backgroundColor: string;
+  barStyle: any;
+};
+
+const CustomStatusBar = (
+  {
+    backgroundColor,
+    barStyle,
+    //add more props StatusBar
+  }: CustomStatusBarProps
+): React.ReactElement => { 
+   
+   const insets = useSafeAreaInsets();
+
+   return (
+     <View style={{ height: insets.top, backgroundColor }}>
+        <StatusBar
+          animated={true}
+          backgroundColor={backgroundColor}
+          barStyle={barStyle} />
+     </View>
+   );
+}
 
 export default () => {
   const darkMode = false;
 
   return (
-    <>
+    
+    <SafeAreaProvider>
+      {/* <StatusBar
+        barStyle={darkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={darkMode ? '#000000' : '#FFFFFF'}
+      /> */}
+      <CustomStatusBar barStyle={darkMode ? 'light-content' : 'dark-content'} backgroundColor={darkMode ? '#000000' : '#FFFFFF'} />
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider
         {...eva}
@@ -23,6 +54,7 @@ export default () => {
         {/* <AppNavigator /> */}
         <AuthNavigator />
       </ApplicationProvider>
-    </>
+      </SafeAreaProvider>
+    
   );
 };
