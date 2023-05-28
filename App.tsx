e785cfeb7cb4280b05af34267eb0b987bf481319
Javier Sosa <jcarlos.sosagomez@gmail.com>
2023-05-28 20:20:27 -0300

    Format App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,10 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 import { default as theme } from './theme.json';
 import AuthNavigator from './navigation/AuthNavigator';
-import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 import { StatusBar, View } from 'react-native';
 import React from 'react';
 
@@ -13,37 +16,33 @@ type CustomStatusBarProps = {
   barStyle: any;
 };
 
-const CustomStatusBar = (
-  {
-    backgroundColor,
-    barStyle,
-    //add more props StatusBar
-  }: CustomStatusBarProps
-): React.ReactElement => { 
-   
-   const insets = useSafeAreaInsets();
+const CustomStatusBar = ({
+  backgroundColor,
+  barStyle,
+}: //add more props StatusBar
+CustomStatusBarProps): React.ReactElement => {
+  const insets = useSafeAreaInsets();
 
-   return (
-     <View style={{ height: insets.top, backgroundColor }}>
-        <StatusBar
-          animated={true}
-          backgroundColor={backgroundColor}
-          barStyle={barStyle} />
-     </View>
-   );
-}
+  return (
+    <View style={{ height: insets.top, backgroundColor }}>
+      <StatusBar
+        animated={true}
+        backgroundColor={backgroundColor}
+        barStyle={barStyle}
+      />
+    </View>
+  );
+};
 
 export default () => {
   const darkMode = false;
 
   return (
-    
     <SafeAreaProvider>
-      {/* <StatusBar
+      <CustomStatusBar
         barStyle={darkMode ? 'light-content' : 'dark-content'}
         backgroundColor={darkMode ? '#000000' : '#FFFFFF'}
-      /> */}
-      <CustomStatusBar barStyle={darkMode ? 'light-content' : 'dark-content'} backgroundColor={darkMode ? '#000000' : '#FFFFFF'} />
+      />
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider
         {...eva}
@@ -54,7 +53,6 @@ export default () => {
         {/* <AppNavigator /> */}
         <AuthNavigator />
       </ApplicationProvider>
-      </SafeAreaProvider>
-    
+    </SafeAreaProvider>
   );
 };
