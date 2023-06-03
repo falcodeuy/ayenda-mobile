2e7d8a3ea38eab754197cde7192d1a8d4261fece
Javier Sosa <jcarlos.sosagomez@gmail.com>
2023-06-03 18:00:17 -0300

    Feature - Add ForgotPassword screen

--- navigation/SignedOutNavigator.tsx
+++ navigation/SignedOutNavigator.tsx
@@ -1,27 +1,26 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Login from "../screens/Login";
-import SignUp from "../screens/SignUp";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from '../screens/Login';
+import SignUp from '../screens/SignUp';
+import ForgotPassword from '../screens/ForgotPassword/ForgotPassword';
 
 const Stack = createStackNavigator();
 
 const screenOptions = {
-    headerShown: false,
-}
+  headerShown: false,
+};
 
 const SignedOutNavigator: React.FC = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName='Login'
-                screenOptions={screenOptions}
-            >
-                <Stack.Screen name='Login' component={Login} />
-                <Stack.Screen name='SignUp' component={SignUp} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={screenOptions}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default SignedOutNavigator
+export default SignedOutNavigator;
