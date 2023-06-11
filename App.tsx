ad86d75ec808c8e135939df1851574e7006a9f2a
Javier Sosa <jcarlos.sosagomez@gmail.com>
2023-06-11 20:32:01 -0300

    Add Auth's and App's navigator in App navigator container

--- App.tsx
+++ App.tsx
@@ -4,13 +4,16 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 import { default as theme } from './theme.json';
-import AuthNavigator from './navigation/AuthNavigator';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 import { StatusBar, View } from 'react-native';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AppNavigator from './navigation';
+import AuthNavigator from './navigation/AuthNavigator';
 
 type CustomStatusBarProps = {
   backgroundColor: string;
@@ -25,7 +28,7 @@ CustomStatusBarProps): React.ReactElement => {
   const insets = useSafeAreaInsets();
 
   return (
-    <View style={{ height: insets.top, backgroundColor }}>
+    <View style={{ backgroundColor }}>
       <StatusBar
         animated={true}
         backgroundColor={backgroundColor}
@@ -38,6 +41,7 @@ CustomStatusBarProps): React.ReactElement => {
 export default () => {
   const queryClient = new QueryClient();
   const darkMode = false;
+  const Stack = createStackNavigator();
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -53,8 +57,12 @@ export default () => {
             darkMode ? { ...eva.dark, ...theme } : { ...eva.light, ...theme }
           }
         >
-          {/* <AppNavigator /> */}
-          <AuthNavigator />
+          <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="Auth" component={AuthNavigator} />
+              <Stack.Screen name="App" component={AppNavigator} />
+            </Stack.Navigator>
+          </NavigationContainer>
         </ApplicationProvider>
       </SafeAreaProvider>
     </QueryClientProvider>
